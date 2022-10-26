6560076fee48ff652f21fbabab816896e47d3c44
AzharSiddiqui9097 <113844683+AzharSiddiqui9097@users.noreply.github.com>
2022-10-26 11:41:16 +0530

    Update locators.py

--- Locators/locators.py
+++ Locators/locators.py
@@ -3,6 +3,7 @@ nxt_button = "id:NextButton"
 user_name_element = "id:LoginNameTextBox"
 password_element = "id:PasswordTextBox"
 login_button = "xpath://input[@id='LoginButton']"
+admin_button_element = "xpath://*[@id='menuItems']/li[11]/a"
 
 
 
